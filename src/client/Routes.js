7febea7e7c3b85ae731322c5613bdeb46f30e786
black996 <rharasis@yahoo.com>
2022-01-22 19:58:32 +0200

    add UsersList to the routes

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,11 +1,14 @@
 import React from "react";
-import Home from "./components/Home";
 import { Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import UsersList from "./components/UsersList";
 
 const Routes = () => {
   return (
     <div>
       <Route exact path="/" component={Home} />
+      <Route exact path="/users" component={UsersList} />
     </div>
   );
 };
