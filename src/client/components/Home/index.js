f256a5133856d7a65f460252371ee79dd7c28077
black996 <rharasis@yahoo.com>
2022-01-22 19:59:31 +0200

    fix Home component typo

--- src/client/components/Home/index.js
+++ src/client/components/Home/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <div>I am the home component</div>
+      <h3>I am the home component</h3>
       <button onClick={() => alert("hellooo")}>I'll Alert hello</button>
     </div>
   );
