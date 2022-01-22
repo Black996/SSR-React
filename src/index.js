42d7ada08beb081ff17bf6bb491ba42e8922810e
black996 <rharasis@yahoo.com>
2022-01-22 19:56:17 +0200

    add redux Providers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
+import "babel-polyfill";
 import express from "express";
 import renderer from "./helpers/renderer";
+import createStore from "./helpers/reduxServerStore";
 
 const app = express();
 
 app.use(express.static("public"));
 
 app.get("*", (req, res) => {
-  res.send(renderer(req));
+  const store = createStore();
+
+  // We'll do some logic here
+
+  res.send(renderer(req, store));
 });
 
 app.listen(3000, () => console.log("Listening on Port 3000"));
