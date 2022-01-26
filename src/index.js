dc92605dd14f57b40c2c0eb91d38923e027d3457
black996 <rharasis@yahoo.com>
2022-01-26 19:44:52 +0200

    add proxy boilerplate at render server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "babel-polyfill";
 import express from "express";
+import proxy from "express-http-proxy";
 import { matchRoutes } from "react-router-config";
 import renderer from "./helpers/renderer";
 import createStore from "./helpers/reduxServerStore";
@@ -7,6 +8,12 @@ import routes from "./client/Routes";
 
 const app = express();
 
+app.use("/api", proxy("http://react-ssr-api.herokuapp.com"), {
+  proxyReqOptDecorator(opts) {
+    opts.header["x-forwarded-host"] = "localhost:3000";
+    return opts;
+  },
+});
 app.use(express.static("public"));
 
 app.get("*", (req, res) => {
