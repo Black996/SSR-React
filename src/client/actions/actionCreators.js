91787055553ef2b1b79ec6767cb13a02a6ae10c2
black996 <rharasis@yahoo.com>
2022-01-27 18:27:07 +0200

    add actionCreator to get current users and utilize the passed axios instances

--- src/client/actions/actionCreators.js
+++ src/client/actions/actionCreators.js
@@ -1,11 +1,19 @@
-import axios from "axios";
 import { actionTypes } from "./types";
 
-export const fetchUsers = () => async (dispatch) => {
-  const res = await axios.get("http://react-ssr-api.herokuapp.com/users");
+export const fetchUsers = () => async (dispatch, getState, api) => {
+  const res = await api.get("/users");
 
   dispatch({
     type: actionTypes.FETCH_USERS,
+    payload: res,
+  });
+};
+
+export const fetchCurrentUser = () => async (dispatch, getState, api) => {
+  const res = await api.get("/current_user");
+
+  dispatch({
+    type: actionTypes.FETCH_CURRENT_USER,
     payload: res,
   });
 };
