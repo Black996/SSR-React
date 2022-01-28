c242d1c20638640d6e1fd9f61eea305eb81df780
black996 <rharasis@yahoo.com>
2022-01-29 00:11:25 +0200

    manage redirect

--- src/index.js
+++ src/index.js
@@ -37,8 +37,13 @@ app.get("*", (req, res) => {
   Promise.all(promises).then(() => {
     const context = {};
     const content = renderer(req, store, context);
+    if (context.url) {
+      return res.redirect(301, context.url);
+    }
 
-    if (context.notFound) res.status(404);
+    if (context.notFound) {
+      res.status(404);
+    }
     res.send(content);
   });
 });
