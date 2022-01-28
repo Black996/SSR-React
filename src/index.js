77d46a451e0ffab23b8e0e92fdc2cb439f0185b5
black996 <rharasis@yahoo.com>
2022-01-28 17:05:20 +0200

    garuntee first render is done despite any async error

--- src/index.js
+++ src/index.js
@@ -22,11 +22,25 @@ app.use(express.static("public"));
 app.get("*", (req, res) => {
   const store = createStore(req);
 
-  const promises = matchRoutes(routes, req.url).map(({ route }) => {
-    return route.loadData ? route.loadData(store) : null;
-  });
+  const promises = matchRoutes(routes, req.url)
+    .map(({ route }) => {
+      return route.loadData ? route.loadData(store) : null;
+    })
+    .map((promise) => {
+      if (promise) {
+        return new Promise((resolve, reject) => {
+          promise.then(resolve).catch(resolve);
+        });
+      }
+    });
 
-  Promise.all(promises).then(() => res.send(renderer(req, store)));
+  Promise.all(promises).then(() => {
+    const context = {};
+    const content = renderer(req, store, context);
+
+    if (context.notFound) res.status(404);
+    res.send(content);
+  });
 });
 
 app.listen(3000, () => console.log("Listening on Port 3000"));
